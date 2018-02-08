6b71f31e900e037c135a176a5f72fcf2eb0c1279
Aaron John Groeneveld <aaron.groeneveld@gmail.com>
2018-02-07 17:13:10 -0700

    Added first 2
    about 3 questions, no apostrophes

--- code.js
+++ code.js
@@ -2,10 +2,26 @@
 // this is just a stub for a function you need to implement
 //
 function getStats(txt) {
+
+    var nChars = txt.length;
+    if (nChars===0){
+        var nWords = 0
+        var nLines = 0
+    }
+    else{
+        var patt = /[^a-zA-Z0-9]+/g;   //Pattern for RegEx is any alphanumeric and apostrophes (for contractions)
+        var wordList = cleanArray(txt.replace(patt," ").toLowerCase().split(" "))
+        var nWords = wordList.length
+        var nLines = txt.split(/\r\n|\r|\n/).length
+    }
+
+
+
+
     return {
-        nChars: 123,
-        nWords: 22,
-        nLines: 10,
+        nChars: nChars,
+        nWords: nWords,
+        nLines: nLines,
         nNonEmptyLines: 22,
         averageWordLength: 3.3,
         maxLineLength: 33,
@@ -15,3 +31,19 @@ function getStats(txt) {
     };
 }
 
+
+
+
+//Idea taken from here to take off the extra split when puncuation ends a string
+//https://stackoverflow.com/questions/281264/remove-empty-elements-from-an-array-in-javascript
+
+//Really bad solution, but I don't want to spend too much time on it
+function cleanArray(actual) {
+    var newArray = new Array();
+    for (var i = 0; i < actual.length; i++) {
+        if (actual[i]!="") {
+            newArray.push(actual[i]);
+        }
+    }
+    return newArray;
+}
